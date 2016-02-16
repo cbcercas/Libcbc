6f40b85c5a2a15b1bcd808c2a737dbe16dffab70
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-02-16 17:28:59 +0100

    first write of printer_d

--- libftprintf/source/printer_d.c
+++ libftprintf/source/printer_d.c
@@ -1,12 +1,31 @@
 #include <stdarg.h>
+#include <inttypes.h>
 #include "type.h"
+#include "libft.h"
 
 #include <stdio.h>
 
 int ft_print_d(t_args *sarg, va_list *larg)
 {
-	printf("In ft_print_d\n");
-	if (!sarg && !larg)
-		return (-1);
-	return (0);
+	printf("If printer_d\n");
+	intmax_t	nb;
+	char	*str;
+	int		len;
+
+	nb = va_arg(larg, intmax_t);
+	if (sarg->len_modifier == h)
+		str = ft_stoa((short)nb);
+	else if (sarg->len_modifier == l)
+		str = ft_ltoa((long)nb);
+	else if (sarg->len_modifier == ll)
+		str = ft_lltoa((long long)nb);
+	else
+	{
+		printf("other\n");
+		str = ft_itoa((int)nb);
+	}
+	len = ft_strlen(str);
+	ft_putstr(str);
+	//free(str);
+	return (len);
 }
