a44a77ab417a1f5c4b9deed2250dda131ca30a93
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-02-18 10:20:22 +0100

    complete printer_d

--- libftprintf/source/printer_d.c
+++ libftprintf/source/printer_d.c
@@ -2,30 +2,38 @@
 #include <inttypes.h>
 #include "type.h"
 #include "libft.h"
+#include "utils.h"
 
 #include <stdio.h>
-
+static char *ft_dtoa(intmax_t nb)
+{
+	if (sarg->len_modifier == h)
+		return (ft_stoa((short)nb));
+	else if (sarg->len_modifier == l)
+		return (ft_ltoa((long)nb));
+	else if (sarg->len_modifier == ll)
+		return (ft_lltoa((long long)nb));
+	else if (sarg->len_modifier == j)
+		return (ft_imtoa(nb));
+	else if (sarg->len_modifier == z)
+		return (ft_sttoa((size_t)nb));
+	else
+		return (ft_itoa((int)nb));
+}
 int ft_print_d(t_args *sarg, va_list *larg)
 {
-	printf("If printer_d\n");
-	intmax_t	nb;
 	char	*str;
 	int		len;
 
-	nb = va_arg(larg, intmax_t);
-	if (sarg->len_modifier == h)
-		str = ft_stoa((short)nb);
-	else if (sarg->len_modifier == l)
-		str = ft_ltoa((long)nb);
-	else if (sarg->len_modifier == ll)
-		str = ft_lltoa((long long)nb);
-	else
-	{
-		printf("other\n");
-		str = ft_itoa((int)nb);
-	}
-	len = ft_strlen(str);
+	str = ft_dtoa(va_arg(larg, intmax_t));
+	len = ft_strlen(str) + sarg->sign_pos;
+	if (!sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+	if (sarg->sign_pos == 1)
+		ft_putchar('+');
 	ft_putstr(str);
-	//free(str);
+	if (sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, ' ');
+	free(str);
 	return (len);
 }
