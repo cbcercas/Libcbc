a78c492de47bcbf2951072986b3f6d7da751f74c
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-17 11:23:56 +0100

    Move ft_printer_O to printer_o.c

--- libftprintf/source/printer_o.c
+++ libftprintf/source/printer_o.c
@@ -38,3 +38,11 @@ int ft_print_o(t_args *sarg, va_list *larg)
 	free(str);
 	return (len);
 }
+
+int ft_print_O(t_args *sarg, va_list *larg)
+{
+	printf("In ft_print_O\n");
+	if (!sarg && !larg)
+		return (-1);
+	return (-1);
+}
