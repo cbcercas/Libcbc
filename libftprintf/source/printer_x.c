33d6cfec65bc1c59bf649ddfbc7861774cf5a02b
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-17 11:25:58 +0100

    Move ft_printer_X to printer_x.c

--- libftprintf/source/printer_x.c
+++ libftprintf/source/printer_x.c
@@ -3,6 +3,14 @@
 
 #include <stdio.h>
 
+int ft_print_X(t_args *sarg, va_list *larg)
+{
+	printf("In ft_print_X\n");
+	if (!sarg && !larg)
+		return (-1);
+	return (-1);
+}
+
 int ft_print_x(t_args *sarg, va_list *larg)
 {
 	printf("In ft_print_x\n");
