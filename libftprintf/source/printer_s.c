aaae87c7b938289a2251d81278ac6a4ad1e71f76
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-17 11:25:01 +0100

    Move ft_printer_S to printer_s.c

--- libftprintf/source/printer_s.c
+++ libftprintf/source/printer_s.c
@@ -1,10 +1,27 @@
 #include <stdarg.h>
+#include <wchar.h>
 #include "libft.h"
 #include "type.h"
 #include "printer.h"
 #include "utils.h"
 
 #include <stdio.h>
+
+int ft_print_S(t_args *sarg, va_list *larg)
+{
+	printf("In ft_print_S\n");
+	wchar_t *str;
+	size_t len;
+
+	str = va_arg(larg, wchar_t *);
+	len = ft_wcslen(str);
+	if (!sarg->left_pad && (sarg->min_width > len))
+		len += ft_print_pad(len, sarg->min_width, ' ');
+	ft_putwstr(str);
+	if (sarg->left_pad && (sarg->min_width > len))
+		len += ft_print_pad(len, sarg->min_width, ' ');
+	return (len);
+}
 
 int ft_print_s(t_args *sarg, va_list *larg)
 {
