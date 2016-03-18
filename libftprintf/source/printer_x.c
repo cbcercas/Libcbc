76c1297a8c26e589e43fd6a1961c6355936609fa
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-18 21:57:53 +0100

    add %x and %X with padding

--- libftprintf/source/printer_x.c
+++ libftprintf/source/printer_x.c
@@ -1,20 +1,44 @@
 #include <stdarg.h>
 #include "type.h"
-
-#include <stdio.h>
+#include "libft.h"
+#include "utils.h"
 
 int ft_print_X(t_args *sarg, va_list *larg)
 {
-	printf("In ft_print_X\n");
-	if (!sarg && !larg)
-		return (-1);
-	return (-1);
+	char	*str;
+	int		len;
+	uintmax_t nb;
+
+	if (!sarg)
+		return -1;
+	nb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	str = (ft_uitohex(nb, 1));
+	len = ft_strlen(str);
+	if (!sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+	ft_putstr(str);
+	if (sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, ' ');
+	free(str);
+	return (len);
 }
 
 int ft_print_x(t_args *sarg, va_list *larg)
 {
-	printf("In ft_print_x\n");
-	if (!sarg && !larg)
-		return (-1);
-	return (-1);
+	char	*str;
+	int		len;
+	uintmax_t unb;
+
+	if (!sarg)
+		return -1;
+	unb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	str = (ft_uitohex(unb, 0));
+	len = ft_strlen(str);
+	if (!sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+	ft_putstr(str);
+	if (sarg->left_pad && (sarg->min_width > 1))
+		len += ft_print_pad(len, sarg->min_width, ' ');
+	free(str);
+	return (len);
 }
