d0e3b107e27cd8908d1ebcebd4cf387cd39d8754
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-18 21:54:08 +0100

    add padding support for %

--- libftprintf/source/printer_per.c
+++ libftprintf/source/printer_per.c
@@ -1,7 +1,15 @@
 #include "libft.h"
+#include "type.h"
+#include "utils.h"
 
-int ft_print_per(void)
+int ft_print_per(t_args *sarg)
 {
+	size_t len;
+	len = 1;
+	if (!sarg->left_pad && (sarg->min_width > 1))
+			len += ft_print_pad(len, sarg->min_width, ' ');
 	ft_putchar('%');
-	return (1);
+	if (sarg->left_pad && (sarg->min_width > 1))
+			len += ft_print_pad(len, sarg->min_width, ' ');
+	return (len);
 }
