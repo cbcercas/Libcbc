aa365b8925ee0596d78a85e0083100d40f68306b
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-20 09:35:04 +0100

    rewrite printer_o to use octal converter

--- libftprintf/source/printer_o.c
+++ libftprintf/source/printer_o.c
@@ -26,9 +26,11 @@ int ft_print_o(t_args *sarg, va_list *larg)
 {
 	char	*str;
 	int		len;
+	uintmax_t nb;
 	if (!sarg)
 		return -1;
-	str = (ft_uitobase(ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t)), 8));
+	nb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	str = ft_uitooctal(nb, sarg->alternate);
 	len = ft_strlen(str);
 	if (!sarg->left_pad && (sarg->min_width > 1))
 		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
