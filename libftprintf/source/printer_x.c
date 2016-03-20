50afa4e14ba268a00cfcd37d523f5798ccb5704d
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-20 09:40:10 +0100

    correct padding and add 0x prefix

--- libftprintf/source/printer_x.c
+++ libftprintf/source/printer_x.c
@@ -7,15 +7,19 @@ int ft_print_X(t_args *sarg, va_list *larg)
 {
 	char	*str;
 	int		len;
-	uintmax_t nb;
+	uintmax_t unb;
 
-	if (!sarg)
-		return -1;
-	nb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
-	str = (ft_uitohex(nb, 1));
+	unb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	str = (ft_uitohex(unb, 1));
 	len = ft_strlen(str);
+	if (sarg->alternate && ft_strncmp(str, "0\0", 2))
+		len += 2;
+	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && sarg->zero_pad)
+		ft_putstr("0X");
 	if (!sarg->left_pad && (sarg->min_width > 1))
 		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && !sarg->zero_pad)
+		ft_putstr("0X");
 	ft_putstr(str);
 	if (sarg->left_pad && (sarg->min_width > 1))
 		len += ft_print_pad(len, sarg->min_width, ' ');
@@ -29,13 +33,17 @@ int ft_print_x(t_args *sarg, va_list *larg)
 	int		len;
 	uintmax_t unb;
 
-	if (!sarg)
-		return -1;
 	unb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
 	str = (ft_uitohex(unb, 0));
 	len = ft_strlen(str);
+	if (sarg->alternate && ft_strncmp(str, "0\0", 2))
+		len += 2;
+	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && sarg->zero_pad)
+		ft_putstr("0x");
 	if (!sarg->left_pad && (sarg->min_width > 1))
 		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && !sarg->zero_pad)
+		ft_putstr("0x");
 	ft_putstr(str);
 	if (sarg->left_pad && (sarg->min_width > 1))
 		len += ft_print_pad(len, sarg->min_width, ' ');
