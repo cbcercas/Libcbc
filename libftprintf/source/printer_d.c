cc3ae79308f07e7a12a85361d5af33e8c2da1e03
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-03-27 13:07:07 +0200

    add padding support

--- libftprintf/source/printer_d.c
+++ libftprintf/source/printer_d.c
@@ -6,21 +6,49 @@
 
 #include <stdio.h>
 
+static char	*ft_get_nbstr(t_args *sarg, va_list *larg, char *sign)
+{
+	intmax_t	nb;
+	uintmax_t	unb;
+
+	*sign = '+';
+	nb = ft_signed_from_lenght(sarg, va_arg(*larg, intmax_t));
+	if (nb < 0)
+	{
+		unb = (uintmax_t)-nb;
+		*sign = '-';
+	}
+	else
+		unb = (uintmax_t)nb;
+	return (ft_uimtoa(unb));
+}
+
+
 int ft_print_d(t_args *sarg, va_list *larg)
 {
 	char		*str;
+	char		sign;
 	int			len;
-	intmax_t	nb;
 
-	nb = ft_signed_from_lenght(sarg, va_arg(*larg, intmax_t));
-	str = ft_imtoa(nb);
-	len = ft_strlen(str);
+	str = ft_get_nbstr(sarg, larg, &sign);
+	len = ft_strlen(str) + (sign == '-' ? 1 : 0);
 	if (!sarg->left_pad && (sarg->min_width > 1))
-		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
-	if ((sarg->sign_pos || sarg->blank_pos) && nb >= 0)
 	{
-		ft_putchar((sarg->sign_pos ? '+' : ' '));
-		len++;
+		if ((sarg->sign_pos || sarg->blank_pos || sign == '-') && sarg->zero_pad)
+		{
+			ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
+			len += (sign == '-' ? 0 : 1);
+		}
+		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
+		if ((sarg->sign_pos || sarg->blank_pos || sign == '-') && !sarg->zero_pad)
+		{
+			ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
+			len += (sign == '-' ? 0 : 1);
+		}
+	} else if ((sarg->sign_pos || sarg->blank_pos || sign == '-'))
+	{
+		ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
+		len += (sign == '-' ? 0 : 1);
 	}
 	ft_putstr(str);
 	if (sarg->left_pad && (sarg->min_width > 1))
