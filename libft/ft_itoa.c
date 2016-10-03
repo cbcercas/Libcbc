92fc6dcf54f44c4335048593b804232720ce733a
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-10-03 18:37:20 +0200

    move ft_strrev to his own file

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -12,25 +12,6 @@
 
 #include "libft.h"
 
-static char	*ft_strrev(char *str)
-{
-	char	c;
-	int		i;
-	int		len;
-
-	len = ft_strlen(str) - 1;
-	i = 0;
-	while (i < len)
-	{
-		c = str[len];
-		str[len] = str[i];
-		str[i] = c;
-		len--;
-		i++;
-	}
-	return (str);
-}
-
 char		*ft_itoa(int num)
 {
 	char	*str;
