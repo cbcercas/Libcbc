e87d217d5979da8986bd263eb6e554946541788c
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-10-15 23:13:48 +0200

    %x correct precision on 0

--- libftprintf/sources/printer_x.c
+++ libftprintf/sources/printer_x.c
@@ -3,50 +3,50 @@
 #include "libft.h"
 #include "utils.h"
 
-int ft_print_X(t_args *sarg, va_list *larg)
+int ft_print_X(t_args *s, va_list *larg)
 {
 	char	*str;
 	int		len;
 	uintmax_t unb;
 
-	unb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	unb = ft_unsigned_from_lenght(s, va_arg(*larg, uintmax_t));
 	str = (ft_uitohex(unb, 1));
-	len = ft_strlen(str);
-	if (sarg->alternate && ft_strncmp(str, "0\0", 2))
+	len = (s->precision && !s->precision_len && unb == 0) ? 0 : ft_strlen(str);
+	if (s->alternate && ft_strncmp(str, "0\0", 2))
 		len += 2;
-	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && sarg->zero_pad)
+	if (ft_strncmp(str, "0\0", 2) && s->alternate && s->zero_pad && unb > 0)
 		ft_putstr("0X");
-	if (!sarg->left_pad && (sarg->min_width > 1))
-		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
-	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && !sarg->zero_pad)
+	if (!s->left_pad && (s->min_width > 1))
+		len += ft_print_pad(len, s->min_width, s->zero_pad ? '0' : ' ');
+	if (ft_strncmp(str, "0\0", 2) && s->alternate && !s->zero_pad && unb > 0)
 		ft_putstr("0X");
-	ft_putstr(str);
-	if (sarg->left_pad && (sarg->min_width > 1))
-		len += ft_print_pad(len, sarg->min_width, ' ');
+	(s->precision && !s->precision_len && unb == 0) ? 0 : ft_putstr(str);
+	if (s->left_pad && (s->min_width > 1))
+		len += ft_print_pad(len, s->min_width, ' ');
 	free(str);
 	return (len);
 }
 
-int ft_print_x(t_args *sarg, va_list *larg)
+int ft_print_x(t_args *s, va_list *larg)
 {
 	char	*str;
 	int		len;
 	uintmax_t unb;
 
-	unb = ft_unsigned_from_lenght(sarg, va_arg(*larg, uintmax_t));
+	unb = ft_unsigned_from_lenght(s, va_arg(*larg, uintmax_t));
 	str = (ft_uitohex(unb, 0));
-	len = ft_strlen(str);
-	if (sarg->alternate && ft_strncmp(str, "0\0", 2))
+	len = (s->precision && !s->precision_len && unb == 0) ? 0 : ft_strlen(str);
+	if (s->alternate && ft_strncmp(str, "0\0", 2))
 		len += 2;
-	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && sarg->zero_pad)
+	if (ft_strncmp(str, "0\0", 2) && s->alternate && s->zero_pad && unb > 0)
 		ft_putstr("0x");
-	if (!sarg->left_pad && (sarg->min_width > 1))
-		len += ft_print_pad(len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
-	if (ft_strncmp(str, "0\0", 2) && sarg->alternate && !sarg->zero_pad)
+	if (!s->left_pad && (s->min_width > 1))
+		len += ft_print_pad(len, s->min_width, s->zero_pad ? '0' : ' ');
+	if (ft_strncmp(str, "0\0", 2) && s->alternate && !s->zero_pad && unb > 0)
 		ft_putstr("0x");
-	ft_putstr(str);
-	if (sarg->left_pad && (sarg->min_width > 1))
-		len += ft_print_pad(len, sarg->min_width, ' ');
+	(s->precision && !s->precision_len && unb == 0) ? 0 : ft_putstr(str);
+	if (s->left_pad && (s->min_width > 1))
+		len += ft_print_pad(len, s->min_width, ' ');
 	free(str);
 	return (len);
 }
