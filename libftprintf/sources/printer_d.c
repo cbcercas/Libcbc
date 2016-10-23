e973ef89e289b7bd09a3a923f0edd99cabdc9257
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-10-23 21:08:57 +0200

    %d Full rewrite

--- libftprintf/sources/printer_d.c
+++ libftprintf/sources/printer_d.c
@@ -20,9 +20,56 @@ static char	*ft_get_nbstr(t_args *sarg, va_list *larg, char *sign)
 	}
 	else
 		unb = (uintmax_t)nb;
+	if (unb == 0 && sarg->precision)
+		return (ft_strnew(0));
 	return (ft_uimtoa(unb));
 }
 
+static void	put_sign(char *sign, t_args *s, unsigned  int *len, char **nbr)
+{
+	char	*tmp;
+
+	if (s->sign_pos || *sign == '-')
+	{
+		tmp = ft_strjoin((*sign == '-') ? "-" : "+", *nbr);
+		free(*nbr);
+		*nbr = tmp;
+		*len += 1;
+	}
+	else if (s->blank_pos)
+	{
+		tmp = ft_strjoin(" ", *nbr);
+		free(*nbr);
+		*nbr = tmp;
+		*len += 1;
+	}
+}
+
+static void put_precision(t_args *s, unsigned int *len, char **nbr, char *sign)
+{
+	char	*tmp;
+	unsigned int l;
+	unsigned int i;
+	unsigned int w;
+
+	i = 0;
+	if ((s->precision_len > *len) || (s->zero_pad && s->min_width > *len && !s->left_pad))
+	{
+		w = (s->precision_len) ? s->precision_len : s->min_width;
+		l = (s->precision)? s->precision_len - *len : s->min_width - *len;
+		if (s->zero_pad && !s->precision)
+			l -= (*sign == '-' || s->sign_pos) ? 1 : 0;
+		tmp = ft_strnew((s->precision_len)? s->precision_len : s->min_width);
+		while (i < l)
+			tmp[i++] = '0';
+		l = 0;
+		while (i < w)
+			tmp[i++] = nbr[0][l++];
+		free(*nbr);
+		*nbr = tmp;
+		*len = ft_strlen(tmp);
+	}
+}
 
 int ft_print_d(t_args *sarg, va_list *larg)
 {
@@ -31,31 +78,14 @@ int ft_print_d(t_args *sarg, va_list *larg)
 	unsigned	len;
 
 	str = ft_get_nbstr(sarg, larg, &sign);
-	len = ft_strlen(str) + (sign == '-' ? 1 : 0);
-	unsigned len2 = len;
-	if (!sarg->left_pad && (sarg->min_width > 1) && sarg->precision_len < sarg->min_width)
-	{
-		if ((sarg->sign_pos || sarg->blank_pos || sign == '-') && sarg->zero_pad)
-		{
-			ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
-			len += (sign == '-' ? 0 : 1);
-		}
-		len += ft_print_pad((len > sarg->precision_len) ? len : sarg->precision_len, sarg->min_width, sarg->zero_pad ? '0' : ' ');
-		if ((sarg->sign_pos || sarg->blank_pos || sign == '-') && !sarg->zero_pad)
-		{
-			ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
-			len += (sign == '-' ? 0 : 1);
-		}
-	} else if ((sarg->sign_pos || sarg->blank_pos || sign == '-'))
-	{
-		ft_putchar((sarg->sign_pos || sign == '-' ? sign : ' '));
-		len += (sign == '-' ? 0 : 1);
-	}
-	if (sarg->precision && sarg->precision_len)
-		len += ft_print_pad(len2, sarg->precision_len, '0');
-	ft_putstr(str);
-	if (sarg->left_pad && (sarg->min_width > 1))
+	len = ft_strlen(str);
+	put_precision(sarg, &len, &str, &sign);
+	put_sign(&sign, sarg, &len, &str);
+	if(!sarg->left_pad && sarg->precision_len < sarg->min_width && sarg->min_width > len)
 		len += ft_print_pad(len, sarg->min_width, ' ');
-	free(str);
+	if (!(!ft_strcmp(str, "0") && sarg->precision && sarg->precision_len <= len))
+		ft_putstr(str);
+	if (sarg->left_pad && (sarg->min_width > 1))
+	 	len += ft_print_pad(len, sarg->min_width, ' ');
 	return (len);
 }
