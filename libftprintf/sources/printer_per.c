95097f9dcc999de38a6c91611da40542079dab79
Christopher BRAVO CERCAS <christopher@cercas.fr>
2016-10-24 23:33:21 +0200

    %% add zero pad support

--- libftprintf/sources/printer_per.c
+++ libftprintf/sources/printer_per.c
@@ -8,7 +8,7 @@ int	ft_print_per(t_args *sarg)
 
 	len = 1;
 	if (!sarg->left_pad && (sarg->min_width > 1))
-			len += ft_print_pad(len, sarg->min_width, ' ');
+		len += ft_print_pad(len, sarg->min_width, (sarg->zero_pad) ? '0' : ' ');
 	ft_putchar('%');
 	if (sarg->left_pad && (sarg->min_width > 1))
 			len += ft_print_pad(len, sarg->min_width, ' ');
