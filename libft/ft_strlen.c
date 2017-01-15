c1e33f5dac037bdba6181eabf374220ad2272cd9
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-15 09:59:50 +0100

    ft_strlen move counter to a pointer arithmetic

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/08 11:02:51 by chbravo-          #+#    #+#             */
-/*   Updated: 2015/12/09 12:11:48 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/15 09:50:38 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,10 @@
 
 size_t	ft_strlen(const char *str)
 {
-	size_t i;
+	const char	*c;
 
-	i = 0;
-	while (str[0] != '\0')
-	{
-		str++;
-		i++;
-	}
-	return (i);
+	c = str;
+	while (*c)
+		c++;
+	return (c - str);
 }
