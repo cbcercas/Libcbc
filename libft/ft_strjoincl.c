2d13a83b3e6932155dc1a7941a23516bbf8b2952
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-15 09:57:38 +0100

    ft_strjoincl add null string security

--- libft/ft_strjoincl.c
+++ libft/ft_strjoincl.c
@@ -1,25 +1,29 @@
 #include "libft.h"
-#include <stdio.h>
+
 char	*ft_strjoincl(char *s1, char *s2, int free)
 {
 	char	*s;
+	size_t	ls1;
+	size_t	ls2;
 
+	ls1 = (s1) ? ft_strlen(s1) : 0;
+	ls2 = (s2) ? ft_strlen(s2) : 0;
 	if (s1 || s2)
 	{
-		s = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+		s = ft_strnew(ls1 + ls2 + 1);
 		if (s)
-		{	
-			s = ft_strcpy(s, s1);
-			ft_strlcat(s, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+		{
+			if (s1)
+				s = ft_strcpy(s, s1);
+			if (s2)
+				ft_strlcat(s, s2, ls1 + ls2 + 1);
 		}
 		else
 			s = NULL;
 	}
 	else
 		s = NULL;
-	if (free == 1 || free == 3)
-		ft_strdel((char **)&s1);
-	if (free == 2 || free == 3)
-		ft_strdel((char **)&s2);
+	((free == 1 || free == 3) && s1) ? ft_strdel((char **)&s1) : 0;
+	((free == 2 || free == 3) && s2) ? ft_strdel((char **)&s2) : 0;
 	return (s);
 }
