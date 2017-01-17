f93da9fdcbec837be8df0590a8014f50a4470d2c
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-17 16:29:44 +0100

    fix ft_strjoincl NULL string

--- libft/srcs/core/ft_strjoincl.c
+++ libft/srcs/core/ft_strjoincl.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/18 11:45:28 by chbravo-          #+#    #+#             */
-/*   Updated: 2016/11/18 11:45:54 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/17 16:27:56 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,27 @@
 char	*ft_strjoincl(char *s1, char *s2, int free)
 {
 	char	*s;
+	size_t	ls1;
+	size_t	ls2;
 
+	ls1 = (s1) ? ft_strlen(s1) : 0;
+	ls2 = (s2) ? ft_strlen(s2) : 0;
 	if (s1 || s2)
 	{
-		s = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
+		s = ft_strnew(ls1 + ls2 + 1);
 		if (s)
 		{
-			s = ft_strcpy(s, s1);
-			ft_strlcat(s, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
+			if (s1)
+				s = ft_strcpy(s, s1);
+			if (s2)
+				ft_strlcat(s, s2, ls1 + ls2 + 1);
 		}
 		else
 			s = NULL;
 	}
 	else
 		s = NULL;
-	if (free == 1 || free == 3)
-		ft_strdel((char **)&s1);
-	if (free == 2 || free == 3)
-		ft_strdel((char **)&s2);
+	((free == 1 || free == 3) && s1) ? ft_strdel((char **)&s1) : 0;
+	((free == 2 || free == 3) && s2) ? ft_strdel((char **)&s2) : 0;
 	return (s);
 }
