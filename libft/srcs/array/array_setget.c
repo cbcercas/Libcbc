f746f7f34e1e4760c97581bca69ff41cfa521bdb
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-23 23:06:11 +0100

    array: forgot to add getter and setter

--- libft/srcs/array/array_setget.c
+++ libft/srcs/array/array_setget.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/22 15:22:15 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/01/22 15:24:36 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/23 23:05:26 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 void	array_set_at(t_array *array, size_t pos, const void *data)
 {
-	ft_memcpy(array->array + pos * array->elem_size, data, array->elem_size);
+	ft_memcpy(array_get_at(array, pos), data, array->elem_size);
+}
+
+void	*array_get_at(t_array *array, size_t pos)
+{
+	return (array->array + pos * array->elem_size);
+}
+
+void	array_set(t_array *array, size_t pos, const void *data)
+{
+	if (pos >= array->used)
+		return;
+	ft_memcpy(array_get_at(array, pos), data, array->elem_size);
+}
+
+void	*array_get(t_array *array, size_t pos)
+{
+	if (pos >= array->used)
+		return(NULL);
+	return (array->array + pos * array->elem_size);
 }
