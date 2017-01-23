7a306b2a27e863463727df8d008001cac62d7614
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-23 23:11:56 +0100

    array: add array_pop and array_swap function #2 forgot array_swap

---
+++ libft/srcs/array/array_swap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   array_swap.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/23 18:07:19 by chbravo-          #+#    #+#             */
+/*   Updated: 2017/01/23 22:45:00 by chbravo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <array/array.h>
+
+/**
+** @brief      Swap two elements in the array
+**
+** @param      array  The t_array
+** @param[in]  pos_a  The first element position
+** @param[in]  pos_b  The second element position
+**
+** @return     A bool: true on success, false otherwise
+*/
+BOOL	array_swap(t_array *array, size_t pos_a, size_t pos_b)
+{
+	void	*tmp;
+	if ((pos_a >= array->used) || (pos_b >= array->used))
+		return (false);
+	if(!(tmp = ft_memalloc(array->elem_size)))
+		return (false);
+	ft_memcpy(tmp, array_get_at(array, pos_a), array->elem_size);
+	ft_memcpy(array_get_at(array, pos_a), array_get_at(array, pos_b), array->elem_size);
+	ft_memcpy(array_get_at(array, pos_b), tmp, array->elem_size);
+	ft_memdel(&tmp);
+	return (true);
+}
