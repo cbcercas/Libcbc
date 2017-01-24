7a903bdff2135732a414c7f62bd988c0005f0a29
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-01-24 14:54:12 +0100

    array: Fix array_copy check array dest capacity, enlarge if source > dest

--- libft/srcs/array/array_copy.c
+++ libft/srcs/array/array_copy.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/22 15:25:13 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/01/23 23:20:07 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/24 14:54:00 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@
 */
 t_array *array_copy(t_array *dest_array, t_array *src_array)
 {
-	if (dest_array->used > src_array->capacity)
-		if (!(array_growth(dest_array)));
-			return (NULL)
+	if (src_array->used > dest_array->capacity)
+		if (!(array_growth(dest_array)))
+			return (NULL);
 	dest_array->used = src_array->used;
 	return(ft_memmove(dest_array->array, src_array->array, src_array->used * src_array->elem_size));
 }
