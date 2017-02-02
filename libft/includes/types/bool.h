f71b645a399477c509dc4c121ac39265e64cfc92
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-02-02 03:44:29 +0100

    libft: fix redefinition if stdbool if included

--- libft/includes/types/bool.h
+++ libft/includes/types/bool.h
@@ -6,20 +6,22 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/21 22:35:44 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/01/22 23:03:55 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/01/31 21:32:03 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BOOL_H
-#define BOOL_H
+# define BOOL_H
+
 # ifdef __STDBOOL_H
-# define BOOL bool
+#  define BOOL bool
 # else
-# define BOOL t_bool
+#  define BOOL t_bool
 	typedef enum	e_bool
 	{
-	    false = ( 1 == 0 ),
-	    true = ( ! false )
+		false = (1 == 0),
+		true = (!false)
 	}				t_bool;
 # endif
+
 #endif
