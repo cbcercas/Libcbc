2c98e3bec0dbc783093ec306ea094cc3f4907972
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-02-06 03:40:02 +0100

    String: test string_remove fix error message

--- libft/tests/srcs/string/test_string_remove.c
+++ libft/tests/srcs/string/test_string_remove.c
@@ -19,8 +19,8 @@ Test(string_remove, Test_string_remove_0) {
 	str = string_dup(test);
 	cr_expect_eq(rem = string_remove(str, str->len + 2, 5), 0, "return needed is 0 but your function return %zu", rem);
 	rem = string_remove(str, 5, 6);
-	cr_expect_eq(rem, 6, "return needed is 5 but your function return %zu", rem);
-	cr_expect_eq(str->len, 27, "str->len needed to be 28 after remove, but you have %zu", str->len);
-	cr_expect_eq(rem = string_remove(str, 20, 10), 7, "return needed is 9 but your function return %zu", rem);
+	cr_expect_eq(rem, 6, "return needed is 6 but your function return %zu", rem);
+	cr_expect_eq(str->len, 27, "str->len needed to be 27 after remove, but you have %zu", str->len);
+	cr_expect_eq(rem = string_remove(str, 20, 10), 7, "return needed is 7 but your function return %zu", rem);
 	string_del(&str);
 }
