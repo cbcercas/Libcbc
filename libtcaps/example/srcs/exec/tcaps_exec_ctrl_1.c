1d377766aceb80054cd87bd4555f9f6191705c04
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-03-27 11:56:50 +0200

    Example: Fix newline in ctrl-c

--- libtcaps/example/srcs/exec/tcaps_exec_ctrl_1.c
+++ libtcaps/example/srcs/exec/tcaps_exec_ctrl_1.c
@@ -6,7 +6,7 @@
 /*   By: chbravo- <chbravo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/17 15:47:35 by chbravo-          #+#    #+#             */
-/*   Updated: 2017/02/17 18:50:01 by chbravo-         ###   ########.fr       */
+/*   Updated: 2017/03/27 11:55:08 by chbravo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <tcaps_test.h>
@@ -15,6 +15,7 @@ BOOL	exec_ctrl_c(const t_key *key)
 {
 	(void)key;
 	tputs(tgetstr("cr", NULL), 0, &ft_putchar2);
+	tputs("\n", 0, &ft_putchar2);
 	tputs(tgetstr("cd", NULL), 0, &ft_putchar2);
 	return (false);
 }
