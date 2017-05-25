a2d05972c4da07d1ca25f77f300dbb1bab3419a2
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-25 07:55:01 +0200

    Fix tcaps_moving

--- libtcaps/srcs/tcaps_moving.c
+++ libtcaps/srcs/tcaps_moving.c
@@ -3,19 +3,36 @@
 void	move_cursor_left(t_cpos *cpos, t_ts *ts)
 {
 	log_dbg3("Move cursor left");
-	if (cpos->cp_col <= 1)
-		tputs(tgoto(tgetstr("cm", NULL), ts->ts_cols, cpos->cp_line - 2), 1, &ft_putchar2);
+	if (cpos->cp_col == 0)
+	{
+		tputs(tgetstr("up", NULL), 0, &ft_putchar2);
+		while(cpos->cp_col + 1 < ts->ts_cols)
+		{
+			tputs(tgetstr("nd", NULL), 0, &ft_putchar2);
+			cpos->cp_col += 1;
+		}
+		cpos->cp_line -= 1;
+	}
 	else
+	{
 		tputs(tgetstr("le", NULL), 0, &ft_putchar2);
-	*cpos = get_cursor_pos();
+		cpos->cp_col -= 1;
+	}
 }
 
 void	move_cursor_right(t_cpos *cpos, t_ts *ts)
 {
-	log_dbg3("Move cursor left");
-	if (cpos->cp_col == ts->ts_cols)
-		tputs(tgoto(tgetstr("cm", NULL), 1, cpos->cp_line + 1), 1, &ft_putchar2);
+	log_dbg3("Move cursor right");
+	if (cpos->cp_col + 1 == ts->ts_cols)
+	{
+		tputs(tgetstr("do", NULL), 0, &ft_putchar2);
+		tputs(tgetstr("cr", NULL), 0, &ft_putchar2);
+		cpos->cp_col = 0;
+		cpos->cp_line += 1;
+	}
 	else
+	{
 		tputs(tgetstr("nd", NULL), 0, &ft_putchar2);
-	*cpos = get_cursor_pos();
+		cpos->cp_col += 1;
+	}
 }
