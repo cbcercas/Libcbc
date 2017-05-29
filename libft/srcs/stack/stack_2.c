3279c1a60e098ef97a1cf421861f3b8ff64462a8
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-05-29 21:26:14 +0200

    [FIX]Stack: fix stack_destroy prototype

--- libft/srcs/stack/stack_2.c
+++ libft/srcs/stack/stack_2.c
@@ -22,7 +22,7 @@ size_t		get_stack_length(t_stack *st)
 	return ((st->used) ? st->used - 1 : 0);
 }
 
-void		stack_destroy(t_stack *st)
+void		stack_destroy(t_stack **st)
 {
 	return (array_destroy((t_array **)st));
 }
