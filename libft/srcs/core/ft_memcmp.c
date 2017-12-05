4005a772b29005e49d7fb059f59e31a40cc2bf3a
Christopher BRAVO CERCAS <christopher@cercas.fr>
2017-12-05 18:09:25 +0100

    [FIX] ft_memcmp

--- libft/srcs/core/ft_memcmp.c
+++ libft/srcs/core/ft_memcmp.c
@@ -14,15 +14,17 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char str1;
-	unsigned char str2;
+	unsigned char *str1;
+	unsigned char *str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	while (n--)
 	{
-		str1 = *(unsigned char *)s1++;
-		str2 = *(unsigned char *)s2++;
-		if (str1 != str2)
-			return (str1 - str2);
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
 	}
 	return (0);
 }
